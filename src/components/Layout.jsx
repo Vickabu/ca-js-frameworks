0f9ac22a53d77b884389eafe616c645082bdd999
annechh <142426482+annechh@users.noreply.github.com>
2025-02-27 13:09:09 +0100

    Route update

---
+++ src/components/Layout.jsx
@@ -0,0 +1,5 @@
+function Layout() {
+  return <h1>Layout Component</h1>;
+}
+
+export default Layout;
---
+++ src/pages/CheckoutSuccessPage.jsx
@@ -0,0 +1,5 @@
+function CheckoutSuccessPage() {
+  return <h1>Checkout Success Page</h1>;
+}
+
+export default CheckoutSuccessPage;
