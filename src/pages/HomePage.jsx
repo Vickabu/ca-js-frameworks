dfe47c6a9e419f7fa77a0e2ce2a90afa9bd06a1f
annechh <142426482+annechh@users.noreply.github.com>
2025-02-27 16:37:37 +0100

    Adds list of products home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from 'react'
+import Ratings from '../components/Ratings';
+
+const url = 'https://v2.api.noroff.dev/online-shop';
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  const [products, setProducts] = useState([]);
+  console.log('products',products);
+  
+  useEffect(() => {
+    async function getProducts() {
+      const response = await fetch(url);
+      const data = await response.json();
+      
+      setProducts(data.data);
+    }
+    getProducts()
+  }, []);
+  
+  return (
+    <div className='grid grid-cols-3 gap-4 mt-10'>
+      {products.map((product) => {
+        return (
+          <div key={product.id} className='border rounded'>
+            <div className='max-w-[320px] h-[210px] overflow-hidden'>
+              <img src={product.image.url} alt={product.title} className='w-full h-full object-cover'/>
+            </div>
+            <div>
+              <h2>{product.title}</h2>
+              <p>${product.price}</p>
+              <p>${product.discountedPrice ?? product.price}</p>
+              <Ratings rating={product.rating} />
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+  
 }
 
 export default HomePage;
