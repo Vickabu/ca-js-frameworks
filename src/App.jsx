19301c63f232f2b11cfbc0897fc7f18509a5c1f0
Andreas S. Joakimsen <andjoa00220@stud.noroff.no>
2025-02-27 12:31:10 +0100

    Added routes for every page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { BrowseRouter as Router, Routes, Route } from "react-router-dom"
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import ContactPage from "./pages/CheckoutPage";
+import CheckoutSuccessPage from "./pages/CheckoutSuccessPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 import './App.css';
 
 function App() {
-
   return (
-    <>
+    <Router>
+      <Layout>
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/checkout-success" element={<CheckoutSuccessPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Layout>
     
-    </>
+    </Router>
   );
 }
 
