81d93ed1e7c953e2a2c989027adfb1f8e83d6764
annechh <142426482+annechh@users.noreply.github.com>
2025-03-02 15:26:37 +0100

    Adds show price and discount price when on sale

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,24 +19,39 @@ function HomePage() {
   
   return (
     <div className='grid grid-cols-3 gap-4 mt-10'>
-      {products.map((product) => {
-        return (
-          <div key={product.id} className='border rounded'>
-            <div className='max-w-[320px] h-[210px] overflow-hidden'>
-              <img src={product.image.url} alt={product.title} className='w-full h-full object-cover'/>
+    {products.map((product) => {
+      return (
+        <div key={product.id} className='border rounded bg-white flex flex-col h-full shadow-lg'>
+
+          <div className='w-full h-[210px] overflow-hidden'>
+            <img src={product.image.url} alt={product.title} className='w-full h-full object-cover'/>
+          </div>
+
+          <div className='flex flex-col px-4 py-4  min-h-[180px] flex-grow'>
+
+            <h2 className="text-lg font-semibold">{product.title}</h2>
+
+            <div className='mt-2'>
+              {product.discountedPrice && product.discountedPrice < product.price ? (
+                <p className='flex flex-col'>
+                  <span className='line-through text-gray-500 text-[16px]'>Kr {product.price}</span>
+                  <span className='text-green-600 font-bold text-[20px]'>Kr {product.discountedPrice}</span>
+                </p>
+              ) : (
+                <p className='text-lg font-bold text-[20px]'>Kr {product.price}</p>
+              )}
             </div>
-            <div>
-              <h2>{product.title}</h2>
-              <p>${product.price}</p>
-              <p>${product.discountedPrice ?? product.price}</p>
+
+            <div className='mt-auto'>
               <Ratings rating={product.rating} />
             </div>
+
           </div>
-        )
-      })}
-    </div>
+        </div>
+      )
+    })}
+  </div>
   )
-  
 }
 
 export default HomePage;
