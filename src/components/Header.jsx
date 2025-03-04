64ce66839dcb342e29531f8c13a388fdf7b6dd71
AndreasAUG23F <andabr00198@stud.noroff.no>
2025-03-04 20:45:35 +0100

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,31 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/lazySalesLogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+    
   return (
-    <header className="flex items-center justify-between p-4 border-b border-black bg-[#C8F9C6]">
+    <header className="flex items-center justify-between px-8 py-4 bg-[#C8F9C6]">
       <div className="flex items-center gap-4">
         <img
-          src="/lazy-sales-logo.svg"
+          src={logo}
           alt="Lazy Sales Logo"
-          className="w-12 h-12"
+          className="object-contain"
         />
-        <div className="leading-tight">
-          <h1 className="text-xl font-bold">Lazy Sales</h1>
-          <p className="text-sm">Chill, click, buy</p>
-        </div>
       </div>
-      <nav className="flex items-center gap-8">
+
+      <nav className="flex items-center gap-8 text-black">
         <Link to="/" className="hover:underline">
           Home
         </Link>
         <Link to="/contact" className="hover:underline">
           Contact
         </Link>
+        {}
+        <div className="relative">
+          <FontAwesomeIcon icon={faShoppingCart} className="w-6 h-6" />
+        </div>
       </nav>
     </header>
   );
