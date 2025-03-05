bad553b80a99dcc1b2d3e499aae69e027253b801
annechh <142426482+annechh@users.noreply.github.com>
2025-03-05 12:19:16 +0100

    Style: adds styles to product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -37,34 +37,42 @@ function ProductPage() {
   if (!product) return <h2>Product not found</h2>;
 
   return (
-    <div className="flex">
-      <div className="w-full h-[460px] max-w-[400px] overflow-hidden rounded-t">
-        <img src={product.image.url} alt={product.title} className="w-full h-full object-cover" />
-      </div>
-
-      <div className="max-w-[400px] w-full bg-pink-200">
-        <h1>{product.title}</h1>
-        
-        <div className="mt-2">
-          {product.discountedPrice && product.discountedPrice < product.price ? (
-            <p className="flex flex-col">
-              <span className="line-through text-gray-500 text-[16px]">Kr {product.price}</span>
-              <span className="text-green-600 font-bold text-[20px]">Kr {product.discountedPrice}</span>
-            </p>
-          ) : (
-            <p className="text-lg font-bold text-[20px]">Kr {product.price}</p>
-          )}
+    <div className="flex items-center">
+      <div className="flex flex-col md:flex-row gap-5 md:gap-[50px] xl:gap-[100px] mt-10 mx-5">
+        <div className="w-full h-[350px] md:h-[460px] md:max-w-[400px] overflow-hidden rounded">
+          <img src={product.image.url} alt={product.title} className="w-full h-full object-cover" />
         </div>
 
-        <div className="mt-auto">
-          <Ratings rating={product.rating} />
-        </div>
-        <div>
-          <Button text="Add to Cart" />
+        <div className="flex flex-col max-w-[400px] w-full">
+          <div className="flex flex-col gap-5 pb-2 border-b border-[#BDBDBD]">
+            <h1 className="text-[18px] md:text-[24px] lg:text-[32px] font-bold">{product.title}</h1>
+            <p className="text-[12px] md:text-[16px] lg:text-[18px]">{product.description}</p>
+          </div>
+          <div className="flex items-center justify-between mt-2">
+            {/* temp reviews */}
+            <h2 className="text-[12px] md:text-[16px] lg:text-[18px]">Reviews</h2>
+            <Ratings rating={product.rating} />
+          </div>  
+            
+          <div className="flex flex-col gap-3 items-end mt-auto">
+            <div className="text-right">
+              {product.discountedPrice && product.discountedPrice < product.price ? (
+                <p className="flex flex-col">
+                  <span className="line-through text-[#BDBDBD] text-[12px] md:text-[14px] lg:text-[16px]">Kr {product.price}</span>
+                  <span className="text-green-600 font-bold text-[16px] md:text-[18px] lg:text-[20px]">Kr {product.discountedPrice}</span>
+                </p>
+              ) : (
+                <p className="text-lg font-bold text-[20px]">Kr {product.price}</p>
+              )}
+            </div>
+            <div>
+              <Button text="Add to Cart" />
+            </div>
+          </div>
         </div>
       </div>
-      
     </div>
+    
   );
 }
 
