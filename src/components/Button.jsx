f4e5b29c8927bcc1db3f1b92e4db6cb355ad01ed
Andreas S. Joakimsen <andjoa00220@stud.noroff.no>
2025-03-05 09:07:37 +0100

    added onClick to buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Button ({ text }) {
-    return <button className="bg-green-400">{ text }</button>;
+function Button ({ text, onClick }) {
+    return <button onClick={onClick} className="bg-green-400">{ text }</button>;
 }
 
 export default Button;
