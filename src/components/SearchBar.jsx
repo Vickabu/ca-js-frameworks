be8508361ccc4d4dd952b791e613500ba2b4a418
annechh <142426482+annechh@users.noreply.github.com>
2025-03-06 09:31:10 +0100

    Style: adds focus style to search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,12 +2,18 @@ import PropTypes from "prop-types";
 
 const SearchBar = ({ onSearch }) => {
   return (
-    <input
-      type="text"
-      placeholder="Search products..."
-      className="w-full p-2 border rounded border-gray-400"
-      onChange={(e) => onSearch(e.target.value)}
-    />
+    <form>
+      <label >
+        <input
+          id="search"
+          type="text"
+          placeholder="Search products..."
+          className="w-full p-2 border rounded border-gray-400 focus:bg-[#C8F9C6] focus:outline-0"
+          onChange={(e) => onSearch(e.target.value)}
+        />
+      </label>
+    </form>
+    
   );
 };
 
