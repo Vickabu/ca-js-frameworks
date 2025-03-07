2d43d452d7d49ec9f587ea3b32962f89681e047d
annechh <142426482+annechh@users.noreply.github.com>
2025-03-07 19:28:16 +0100

    Added a div around product

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -52,47 +52,49 @@ function ProductPage() {
   if (!product) return <h2>Product not found</h2>;
 
   return (
-    <div className="flex items-center p-5 shadow-xl mt-4 rounded">
-      <div className="flex flex-col md:flex-row gap-5 md:gap-[50px] xl:gap-[100px] mt-10 mx-5">
-        <div className="w-full h-[350px] md:h-[460px] md:max-w-[400px] overflow-hidden rounded">
-          <img src={product.image.url} alt={product.title} className="w-full h-full object-cover" />
-        </div>
+    <div className="flex items-center">
+      <div className="py-5 md:p-5 shadow-xl rounded">
+        <div className="flex flex-col md:flex-row gap-5 md:gap-[50px] xl:gap-[100px] mt-5 md:mt-10 mb-5 mx-5">
+          <div className="w-full h-[350px] md:h-[460px] md:max-w-[400px] overflow-hidden rounded">
+            <img src={product.image.url} alt={product.title} className="w-full h-full object-cover" />
+          </div>
 
-        <div className="flex flex-col max-w-[400px] w-full">
-          <div className="flex flex-col gap-5 pb-2 border-b border-[#BDBDBD]">
-            <h1 className="text-[18px] md:text-[24px] lg:text-[32px] font-bold">{product.title}</h1>
-            <p className="text-[12px] md:text-[16px] lg:text-[18px]">{product.description}</p>
+          <div className="flex flex-col max-w-[400px] w-full">
+            <div className="flex flex-col gap-5 pb-2 border-b border-[#BDBDBD]">
+              <h1 className="text-[18px] md:text-[24px] lg:text-[32px] font-bold">{product.title}</h1>
+              <p className="text-[12px] md:text-[16px] lg:text-[18px]">{product.description}</p>
+            </div>
+            <div className="flex items-center justify-between mt-2">
+              <Reviews reviews={product.reviews}/>
+              <div className="flex mb-auto">
+                <Ratings rating={product.rating}/>
+              </div>
+            </div>  
+              
+            <div className="flex flex-col gap-3 items-end mt-auto">
+              <div className="text-right">
+                {product.discountedPrice && product.discountedPrice < product.price ? (
+                  <p className="flex flex-col">
+                    <span className="line-through text-[#BDBDBD] text-[12px] md:text-[14px] lg:text-[16px]">Kr {product.price}</span>
+                    <span> - {Math.round(((product.price - product.discountedPrice) / product.price) * 100)}%</span>
+                    <span className="text-green-600 font-bold text-[16px] md:text-[18px] lg:text-[20px]">Kr {product.discountedPrice}</span>
+                  </p>
+                ) : (
+                  <p className="text-lg font-bold text-[20px]">Kr {product.price}</p>
+                )}
+              </div>
+              <div>
+                <Button text="Add to Cart" onClick={handleAddToCart}
+                variant="primary"
+                className="w-full"
+                />
+              </div>
+              {cartQuantity === 0 ? null : <p className="text-[12px] md:text-[16px] lg:text-[18px]">Added to cart: {cartQuantity}</p>}
+            </div>
           </div>
-          <div className="flex items-center justify-between mt-2">
-            <Reviews reviews={product.reviews}/>
-            <div className="flex mb-auto">
-              <Ratings rating={product.rating}/>
-            </div>
-          </div>  
-            
-          <div className="flex flex-col gap-3 items-end mt-auto">
-            <div className="text-right">
-              {product.discountedPrice && product.discountedPrice < product.price ? (
-                <p className="flex flex-col">
-                  <span className="line-through text-[#BDBDBD] text-[12px] md:text-[14px] lg:text-[16px]">Kr {product.price}</span>
-                  <span> - {Math.round(((product.price - product.discountedPrice) / product.price) * 100)}%</span>
-                  <span className="text-green-600 font-bold text-[16px] md:text-[18px] lg:text-[20px]">Kr {product.discountedPrice}</span>
-                </p>
-              ) : (
-                <p className="text-lg font-bold text-[20px]">Kr {product.price}</p>
-              )}
-            </div>
-            <div>
-              <Button text="Add to Cart" onClick={handleAddToCart}
-              variant="primary"
-              className="w-full"
-              />
-            </div>
-            {cartQuantity === 0 ? null : <p className="text-[12px] md:text-[16px] lg:text-[18px]">Added to cart: {cartQuantity}</p>}
-          </div>
-        </div>
-      </div>    
-    </div>    
+        </div>    
+      </div>  
+    </div>  
   );
 }
 
